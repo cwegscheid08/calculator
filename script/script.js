4dc32050aaa4c9d050dd27163c0b620baf29338a
Chris Wegscheid <cwegscheid08@outlook.com>
2018-09-06 15:38:38 -0400

    Set Up Add, Subtract, Multiply, Divide Functions Inside of Operate Function

--- script/script.js
+++ script/script.js
@@ -1,3 +1,36 @@
-function helloWorld() {
-    alert("Hello, World!");
+function operate(funct, ...num) {
+    var functCall = operate.arguments[0];
+    num = [...num];
+
+    if(functCall == "add") {
+        return add(num);
+    } else if(functCall == "subtract") {
+        return subtract(num);
+    } else if(functCall == "multiply") {
+        return multiply(num);
+    } else if (functCall == "divide") {
+        return divide(num);
+    }
+
+    function add() {
+        const addNum = num.reduce((sum,num)=>sum += num);
+        return addNum;
+    }
+
+    function subtract() {
+        const subtractNum = num.reduce((diff,num)=>diff -= num);
+        return subtractNum;
+    }
+
+    function multiply() {
+        const multiplyNum = num.reduce((prdct,num)=>prdct *= num);
+        return multiplyNum;
+    }
+
+    function divide() {
+        const divideNum = num.reduce((rslt,num)=>rslt = (rslt / num));
+        return divideNum;
+    }
+    console.log(functCall);
+    return add(num);
 }
