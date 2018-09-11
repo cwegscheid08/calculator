7610ba9d85b0431c804a0dc86c21d8d3b92633a9
Chris Wegscheid <cwegscheid08@outlook.com>
2018-09-11 09:39:58 -0400

    Basic Page Layout Set, Button Click Updates Display

--- script/script.js
+++ script/script.js
@@ -1,4 +1,39 @@
-function operate(funct, ...num) {
+var container = document.getElementById("container");
+var output = document.getElementById("output");
+var anchor = document.querySelectorAll("a");
+var button = document.getElementById("button");
+
+
+
+
+
+var enter = document.querySelector(".enter");
+var clear = document.querySelector(".clear");
+var displayValue = [0];
+var numButton = document.querySelectorAll(".num");
+anchor = Array.from(anchor);
+
+
+
+for(let key in anchor) {
+    let btn = anchor[key];
+    btn.addEventListener("click", function() {
+        outputVal(btn);
+    })
+}
+
+
+
+function operate(...number) {
+    console.log(number);
+
+    for(let key in number) {
+        
+    }
+
+
+
+
     var functCall = operate.arguments[0];
     num = [...num];
 
@@ -34,3 +69,44 @@ function operate(funct, ...num) {
     console.log(functCall);
     return add(num);
 }
+
+
+
+
+function outputVal(val) {
+    var string = output.textContent;
+    var buttonVal = numButton.value;
+    let last = string.charAt(string.length-1);
+
+    
+    val = val.text;
+
+    
+
+    if(output.textContent == "0") {
+        output.textContent = val;
+        displayValue.push(val);
+    } else if(last != ".") {
+        output.textContent += val;
+        displayValue.push(val);
+    } else if(last == "." && val != ".") {
+        output.textContent += val;
+        displayValue.push(val);
+    }
+    
+    
+    
+    return displayValue;
+}
+
+
+
+clear.addEventListener("click", function() {
+    output.textContent = 0;
+    displayValue = [];
+})
+
+enter.addEventListener("click", function() {
+    displayValue = displayValue.slice(1, displayValue.length - 1);
+    return operate(displayValue);
+})
